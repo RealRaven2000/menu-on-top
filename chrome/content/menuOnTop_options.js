f2310a064fdecb2487c4baa1f8a37385aa24bf58
axel.grude <axel.grude@gmail.com>
2014-11-25 08:58:35 +0000

    Added hover/active support. To do: most themes.

--- chrome/content/menuOnTop_options.js
+++ chrome/content/menuOnTop_options.js
@@ -85,8 +85,12 @@ MenuOnTop.Options = {
 				setElementValue('txtNegativeMargin', MenuOnTop.defaultPREFS.negativeMargin);
 				setElementValue('txtMenuMarginTop', MenuOnTop.defaultPREFS.menuMarginTop);
 				setElementValue('chkMenuShadow', MenuOnTop.defaultPREFS.textShadow);
-				setElementValue('txtMenuBackground',  MenuOnTop.defaultPREFS.menuBackground);
-				setElementValue('txtMenuFontColor',  MenuOnTop.defaultPREFS.menuFontColor);
+				setElementValue('txtMenuBackgroundDefault',  MenuOnTop.defaultPREFS.menuBackground);
+				setElementValue('txtMenuFontColorDefault',  MenuOnTop.defaultPREFS.menuFontColor);
+				setElementValue('txtMenuBackgroundHover',  MenuOnTop.defaultPREFS.menuBackgroundHover);
+				setElementValue('txtMenuFontColorHover',  MenuOnTop.defaultPREFS.menuFontColorHover);
+				setElementValue('txtMenuBackgroundActive',  MenuOnTop.defaultPREFS.menuBackgroundActive);
+				setElementValue('txtMenuFontColorActive',  MenuOnTop.defaultPREFS.menuFontColorActive);
 				setElementValue('txtMenuBorderColor', 'transparent');
 				setElementValue('txtMenuBorderWidth', MenuOnTop.defaultPREFS.menuBorderWidth);
 				setElementValue('txtMaxHeight', MenuOnTop.defaultPREFS.maxHeight);
@@ -97,8 +101,13 @@ MenuOnTop.Options = {
 				setElementValue('chkMenuShadow', true);
 				// from Bloomind's TT deepdark, slighly tweaked start point to make it brighter & more apparent (we are missing borders)
 				setElementValue('txtNegativeMargin', 6);
-				setElementValue('txtMenuBackground',  'linear-gradient(rgb(88, 88, 88), rgb(35, 35, 35) 45%, rgb(33, 33, 33) 48%, rgb(24, 24, 24))'); 
-				setElementValue('txtMenuFontColor', 'rgba(220, 220, 220, 0.8)');
+				setElementValue('txtMenuBackgroundDefault',  'linear-gradient(rgb(88, 88, 88), rgb(35, 35, 35) 45%, rgb(33, 33, 33) 48%, rgb(24, 24, 24))'); 
+				setElementValue('txtMenuFontColorDefault', 'rgba(220, 220, 220, 0.8)');
+        // text-shadow: 0px 0px 3px rgb(0, 173, 238);
+				setElementValue('txtMenuBackgroundHover',  'linear-gradient(rgb(88, 88, 88), rgb(35, 35, 35) 45%, rgb(33, 33, 33) 48%, rgb(24, 24, 24))'); 
+				setElementValue('txtMenuFontColorHover', 'rgb(0, 173, 238)'); // blue on hover
+				setElementValue('txtMenuBackgroundActive',  'linear-gradient(rgb(45,45,45))'); 
+				setElementValue('txtMenuFontColorActive', 'rgba(255, 255, 255, 0.9)');
 				setElementValue('txtMenuBorderColor', 'transparent');
 				setElementValue('txtMenuBorderWidth', '0');
 				setElementValue('txtMaxHeight', 22);
@@ -111,8 +120,12 @@ MenuOnTop.Options = {
 				setElementValue('txtNegativeMargin', 2);
 				setElementValue('txtMenuRadius', '0.5');
 				setElementValue('chkMenuShadow', false);
-				setElementValue('txtMenuBackground',  'linear-gradient(to bottom, rgba(255, 255, 255, 0.85), rgba(215, 215, 215, 0.9))');
-				setElementValue('txtMenuFontColor', 'rgb(15,15,15)');
+				setElementValue('txtMenuBackgroundDefault',  'linear-gradient(to bottom, rgba(255, 255, 255, 0.85), rgba(215, 215, 215, 0.9))');
+				setElementValue('txtMenuFontColorDefault', 'rgb(15,15,15)');
+				setElementValue('txtMenuBackgroundHover',  'linear-gradient(to bottom, rgba(255, 255, 255, 0.85), rgba(215, 215, 215, 0.9))');
+				setElementValue('txtMenuFontColorHover', 'rgb(15,15,15)');
+				setElementValue('txtMenuBackgroundActive',  'linear-gradient(to bottom, rgba(255, 255, 255, 0.85), rgba(215, 215, 215, 0.9))');
+				setElementValue('txtMenuFontColorActive', 'rgb(15,15,15)');
 				setElementValue('txtMenuBorderColor', 'transparent');
 				setElementValue('txtMenuBorderWidth', '0');
 				setElementValue('txtMaxHeight', 20);
@@ -123,8 +136,8 @@ MenuOnTop.Options = {
 				setElementValue('txtNegativeMargin', 2);
 				setElementValue('txtMenuMarginTop', 5);
 				setElementValue('chkMenuShadow', false);
-				setElementValue('txtMenuBackground',  'linear-gradient(#f4f4f9,#c0c1ca)');
-				setElementValue('txtMenuFontColor', 'rgb(0, 0, 0)');
+				setElementValue('txtMenuBackgroundDefault',  'linear-gradient(#f4f4f9,#c0c1ca)');
+				setElementValue('txtMenuFontColorDefault', 'rgb(0, 0, 0)');
 				setElementValue('txtMenuBorderColor', 'transparent');
 				setElementValue('txtMenuBorderWidth', '0');
 				setElementValue('txtMaxHeight', 22);
@@ -133,8 +146,8 @@ MenuOnTop.Options = {
 				break;
 			case 4:  // orange - Lantana
 				setElementValue('chkMenuShadow', true);
-				setElementValue('txtMenuBackground',  'linear-gradient(to bottom, rgba(252,219,143,0.9) 0%,rgba(249,156,62,0.9) 49%,rgba(229,143,45,0.9) 52%,rgba(234,122,37,1) 100%)');
-				setElementValue('txtMenuFontColor', 'rgba(84,84,100,0.9)');
+				setElementValue('txtMenuBackgroundDefault',  'linear-gradient(to bottom, rgba(252,219,143,0.9) 0%,rgba(249,156,62,0.9) 49%,rgba(229,143,45,0.9) 52%,rgba(234,122,37,1) 100%)');
+				setElementValue('txtMenuFontColorDefault', 'rgba(84,84,100,0.9)');
 				setElementValue('txtMenuBorderColor', 'transparent');
 				setElementValue('txtMenuBorderWidth', '0');
 				setElementValue('txtMaxHeight', 22);
@@ -146,8 +159,8 @@ MenuOnTop.Options = {
 				setElementValue('txtMenuMarginTop', 8);
 				setElementValue('chkMenuShadow', true);
 				setElementValue('chkMenubarTransparent', true);
-				setElementValue('txtMenuBackground',  'linear-gradient(to bottom, rgba(234,225,209,0.9) 0%,rgba(232,217,195,0.93) 34%,rgba(229,198,156,1) 100%)');
-				setElementValue('txtMenuFontColor', 'rgb(127, 83, 44)');
+				setElementValue('txtMenuBackgroundDefault',  'linear-gradient(to bottom, rgba(234,225,209,0.9) 0%,rgba(232,217,195,0.93) 34%,rgba(229,198,156,1) 100%)');
+				setElementValue('txtMenuFontColorDefault', 'rgb(127, 83, 44)');
 				setElementValue('txtMenuBorderColor', 'transparent');
 				setElementValue('txtMenuBorderWidth', '0');
 				setElementValue('txtMaxHeight', 24);
@@ -158,8 +171,8 @@ MenuOnTop.Options = {
 				setElementValue('txtNegativeMargin', 2);
 				setElementValue('txtMenuMarginTop', 12);
 				setElementValue('chkMenuShadow', false);
-				setElementValue('txtMenuBackground',  'linear-gradient(to bottom, rgba(255,255,255,1) 0%,rgba(184,185,197,1) 27%,rgba(184,185,197,1) 48%,rgba(210,212,219,1) 65%,rgba(245,245,255,1) 100%)');
-				setElementValue('txtMenuFontColor', 'rgb(0,0,0)');
+				setElementValue('txtMenuBackgroundDefault',  'linear-gradient(to bottom, rgba(255,255,255,1) 0%,rgba(184,185,197,1) 27%,rgba(184,185,197,1) 48%,rgba(210,212,219,1) 65%,rgba(245,245,255,1) 100%)');
+				setElementValue('txtMenuFontColorDefault', 'rgb(0,0,0)');
 				setElementValue('txtMenuBorderColor', 'transparent');
 				setElementValue('txtMenuBorderWidth', '0');
 				setElementValue('txtMaxHeight', 26);
@@ -171,8 +184,8 @@ MenuOnTop.Options = {
 				setElementValue('txtMenuMarginTop', 6);
 				setElementValue('txtMenuRadius', '0.5');
 				setElementValue('chkMenuShadow', false);
-				setElementValue('txtMenuBackground',  'linear-gradient(to bottom, rgba(228, 208, 155, 0.95), rgba(228, 208, 155, 0.5))');
-				setElementValue('txtMenuFontColor', 'rgb(15,15,15)');
+				setElementValue('txtMenuBackgroundDefault',  'linear-gradient(to bottom, rgba(228, 208, 155, 0.95), rgba(228, 208, 155, 0.5))');
+				setElementValue('txtMenuFontColorDefault', 'rgb(15,15,15)');
 				setElementValue('txtMenuBorderColor', 'transparent');
 				setElementValue('txtMenuBorderWidth', '0');
 				setElementValue('txtMaxHeight', 21);
@@ -183,8 +196,8 @@ MenuOnTop.Options = {
 				setElementValue('txtNegativeMargin', 0);
 				setElementValue('txtMenuMarginTop', 2);
 				setElementValue('chkMenuShadow', true);
-				setElementValue('txtMenuBackground',  'linear-gradient(to bottom, rgba(255, 255, 255, 0.85), rgba(215, 215, 215, 0.9))');
-				setElementValue('txtMenuFontColor', 'rgb(15,15,15)');
+				setElementValue('txtMenuBackgroundDefault',  'linear-gradient(to bottom, rgba(255, 255, 255, 0.85), rgba(215, 215, 215, 0.9))');
+				setElementValue('txtMenuFontColorDefault', 'rgb(15,15,15)');
 				setElementValue('txtMenuBorderColor', 'transparent');
 				setElementValue('txtMenuBorderWidth', '0');
 				setElementValue('txtMaxHeight', 20);
@@ -196,8 +209,8 @@ MenuOnTop.Options = {
 				setElementValue('txtMenuMarginTop', 7);
 				setElementValue('txtMenuRadius', '0.5');
 				setElementValue('chkMenuShadow', true);
-				setElementValue('txtMenuBackground',  'linear-gradient(to bottom, rgba(237,4,8,0.8) 0%,rgba(137,13,2,0.8) 45%,rgba(117,23,0,0.8) 100%)');
-				setElementValue('txtMenuFontColor', 'rgba(255, 240, 255, 1)');
+				setElementValue('txtMenuBackgroundDefault',  'linear-gradient(to bottom, rgba(237,4,8,0.8) 0%,rgba(137,13,2,0.8) 45%,rgba(117,23,0,0.8) 100%)');
+				setElementValue('txtMenuFontColorDefault', 'rgba(255, 240, 255, 1)');
 				setElementValue('txtMenuBorderColor', 'transparent');
 				setElementValue('txtMenuBorderWidth', '0');
 				setElementValue('txtMaxHeight', 22);
@@ -209,8 +222,8 @@ MenuOnTop.Options = {
 				setElementValue('txtMenuMarginTop', 4);
 				setElementValue('txtMenuRadius', '0.3');
 				setElementValue('chkMenuShadow', false);
-				setElementValue('txtMenuBackground',  'linear-gradient(to bottom, rgba(206,206,206,1) 0%,rgba(212,212,212,1) 45%,rgba(206,206,206,1) 45%,rgba(199,199,199,1) 100%)');
-				setElementValue('txtMenuFontColor', 'rgba(0, 0, 25, 1)');
+				setElementValue('txtMenuBackgroundDefault',  'linear-gradient(to bottom, rgba(206,206,206,1) 0%,rgba(212,212,212,1) 45%,rgba(206,206,206,1) 45%,rgba(199,199,199,1) 100%)');
+				setElementValue('txtMenuFontColorDefault', 'rgba(0, 0, 25, 1)');
 				setElementValue('txtMenuBorderColor', 'transparent');
 				setElementValue('txtMenuBorderWidth', '0');
 				setElementValue('txtMaxHeight', 22);
@@ -223,8 +236,8 @@ MenuOnTop.Options = {
 				setElementValue('txtMenuRadius', '0');
 				setElementValue('chkMenuShadow', false);
 				setElementValue('chkMenubarTransparent', true);
-				setElementValue('txtMenuBackground',  'linear-gradient(to bottom, rgba(206,206,206,1) 0%,rgba(212,212,212,1) 45%,rgba(206,206,206,1) 45%,rgba(199,199,199,1) 100%)');
-				setElementValue('txtMenuFontColor', 'rgba(0, 0, 25, 1)');
+				setElementValue('txtMenuBackgroundDefault',  'linear-gradient(to bottom, rgba(206,206,206,1) 0%,rgba(212,212,212,1) 45%,rgba(206,206,206,1) 45%,rgba(199,199,199,1) 100%)');
+				setElementValue('txtMenuFontColorDefault', 'rgba(0, 0, 25, 1)');
 				setElementValue('txtMenuBorderColor', 'transparent');
 				setElementValue('txtMenuBorderWidth', '0');
 				setElementValue('txtMaxHeight', 28);
@@ -236,8 +249,12 @@ MenuOnTop.Options = {
 				setElementValue('txtMenuMarginTop', 5);
 				setElementValue('txtMenuRadius', '0.5');
 				setElementValue('chkMenuShadow', true);
-				setElementValue('txtMenuBackground',  'linear-gradient(to bottom, rgba(4,160,238,0.8) 0%,rgba(2,79,138,0.8) 47%,rgba(0,53,118,0.8) 100%)');
-				setElementValue('txtMenuFontColor', 'rgba(225, 245, 255, 0.95)');
+				setElementValue('txtMenuBackgroundDefault',  'linear-gradient(to bottom, rgba(4,160,238,0.8) 0%,rgba(2,79,138,0.8) 47%,rgba(0,53,118,0.8) 100%)');
+				setElementValue('txtMenuFontColorDefault', 'rgba(225, 245, 255, 0.95)');
+				setElementValue('txtMenuBackgroundHover',  'linear-gradient(to bottom, rgba(4,160,238,0.8) 0%,rgba(2,79,138,0.8) 47%,rgba(0,53,118,0.8) 100%)');
+				setElementValue('txtMenuFontColorHover', 'rgba(225, 245, 255, 0.95)');
+				setElementValue('txtMenuBackgroundActive',  'linear-gradient(to bottom, rgba(4,160,238,0.8) 0%,rgba(2,79,138,0.8) 47%,rgba(0,53,118,0.8) 100%)');
+				setElementValue('txtMenuFontColorActive', 'rgba(225, 245, 255, 0.95)');
 				setElementValue('txtMenuBorderColor', 'transparent');
 				setElementValue('txtMenuBorderWidth', '0');
 				setElementValue('txtMaxHeight', 22);
@@ -249,8 +266,8 @@ MenuOnTop.Options = {
 				setElementValue('txtMenuMarginTop', 5);
 				setElementValue('txtMenuRadius', '0.75');
 				setElementValue('chkMenuShadow', true);
-				setElementValue('txtMenuBackground',  'linear-gradient(to bottom, rgba(3,185,173,0.8) 0%,rgba(2,105,108,0.8) 47%,rgba(0,78,92,0.8) 100%)');
-				setElementValue('txtMenuFontColor', 'rgba(225, 255, 250, 0.95)');
+				setElementValue('txtMenuBackgroundDefault',  'linear-gradient(to bottom, rgba(3,185,173,0.8) 0%,rgba(2,105,108,0.8) 47%,rgba(0,78,92,0.8) 100%)');
+				setElementValue('txtMenuFontColorDefault', 'rgba(225, 255, 250, 0.95)');
 				setElementValue('txtMenuBorderColor', 'transparent');
 				setElementValue('txtMenuBorderWidth', '0');
 				setElementValue('txtMaxHeight', 22);
@@ -262,8 +279,8 @@ MenuOnTop.Options = {
 				setElementValue('txtMenuMarginTop', 5);
 				setElementValue('txtMenuRadius', '0.75');
 				setElementValue('chkMenuShadow', true);
-				setElementValue('txtMenuBackground',  'linear-gradient(to bottom, rgba(255,183,107,1) 0%,rgba(255,167,61,1) 11%,rgba(255,124,0,1) 51%,rgba(255,127,4,1) 100%)');
-				setElementValue('txtMenuFontColor', 'rgba(225, 255, 250, 0.99)');
+				setElementValue('txtMenuBackgroundDefault',  'linear-gradient(to bottom, rgba(255,183,107,1) 0%,rgba(255,167,61,1) 11%,rgba(255,124,0,1) 51%,rgba(255,127,4,1) 100%)');
+				setElementValue('txtMenuFontColorDefault', 'rgba(225, 255, 250, 0.99)');
 				setElementValue('txtMenuBorderColor', 'rgba(255,255,255,0.7)');
 				setElementValue('txtMenuBorderWidth', '1px 1px 1px 0px');
 				setElementValue('txtMaxHeight', 22);
