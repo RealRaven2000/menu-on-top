2950f455b7494031c2561e4f32ed547d1a3d972e
RealRaven2000 <axel.grude@gmail.com>
2015-09-15 16:38:12 +0100

    Cranked up max version to 40 added skin folder

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-"C:\Program Files\7-Zip\7z" a -xr!.svn menuOnTop.zip chrome install.rdf icon.png chrome.manifest bootstrap.js
+"C:\Program Files\7-Zip\7z" a -xr!.svn menuOnTop.zip chrome skin install.rdf icon.png chrome.manifest bootstrap.js
 set /P menuOnTopRev=<revision.txt
 set /a menuOnTopRev+=1
 echo %menuOnTopRev% > revision.txt
-move *.xpi "..\Test\0.9.9\"
-rename menuOnTop.zip menuOnTop-0.9.9pre%menuOnTopRev%.xpi
+move *.xpi "..\..\Test\1.0\"
+rename menuOnTop.zip menuOnTop-1.0pre%menuOnTopRev%.xpi
