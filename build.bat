d986e7fc972a3f83c15c83b1eca76e2fc8fbf6da
RealRaven2000 <axel.grude@gmail.com>
2015-10-13 11:44:36 +0100

    1.0

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-"C:\Program Files\7-Zip\7z" a -xr!.svn menuOnTop.zip chrome skin install.rdf icon.png chrome.manifest bootstrap.js
+"C:\Program Files\7-Zip\7z" a -xr!.svn menuOnTop.zip chrome skin avatars install.rdf icon.png chrome.manifest bootstrap.js
 set /P menuOnTopRev=<revision.txt
 set /a menuOnTopRev+=1
 echo %menuOnTopRev% > revision.txt
-move *.xpi "..\..\Test\1.0\"
-rename menuOnTop.zip menuOnTop-1.0pre%menuOnTopRev%.xpi
+move *.xpi "..\..\Test\1.1\"
+rename menuOnTop.zip menuOnTop-1.1pre%menuOnTopRev%.xpi
