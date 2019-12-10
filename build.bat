37752f1088107904ffc11658954c012cf086ae48
RealRaven2000 <axel.grude@gmail.com>
2019-12-10 15:04:41 +0000

    1.16 Released on 10/12/2019

--- build.bat
+++ build.bat
@@ -6,6 +6,6 @@ pwsh -Command "(gc -en UTF8NoBOM install.rdf) -replace 'pre%oldRev%'.trim(), 'pr
 pwsh -Command "(gc -en UTF8NoBOM manifest.json) -replace 'pre%oldRev%'.trim(), 'pre%menuOnTopRev%' | Out-File manifest.json"
 "C:\Program Files\7-Zip\7z" a -xr!.svn menuOnTop.zip avatars chrome skin install.rdf manifest.json icon.png chrome.manifest bootstrap.js
 echo %menuOnTopRev% > revision.txt
-move *.xpi "..\..\Test\1.15\"
+move *.xpi "..\..\Test\1.16\"
 pwsh -Command "Start-Sleep -m 150"
-rename menuOnTop.zip menuOnTop-1.15pre%menuOnTopRev%.xpi
+rename menuOnTop.zip menuOnTop-1.16pre%menuOnTopRev%.xpi
